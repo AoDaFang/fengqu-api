793c8f87de7b2782cdb25b371e24847521fadcf2
AoDaFang <1426490132@qq.com>
2019-04-11 17:50:15 +0800

    首次

---
+++ routes/goods.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var ctrl = require('../controllers/GoodsController')
+
+
+/* restful api */
+
+// router.get('/sendsms', ctrl.sendSMS);
+// router.post('/reg', ctrl.reg);
+//
+// router.post('/like', ctrl.like_add)
+//     .get('/like', ctrl.like_get)
+//     .delete('/like', ctrl.like_delete);
+
+
+router.get('/ltngoods', ctrl.ltngoods)
+
+//添加用户想看   post http://localhost:3000/users/like    ?movie_id=&uid=
+//查询是否想看   get  http://localhost:3000/users/like    ?uid=
+//用户不想看     delete http://localhost:3000/users/like  ?movie_id=&uid=
+
+router.post('/remark',ctrl.add_Remark);//添加评论
+module.exports = router;
